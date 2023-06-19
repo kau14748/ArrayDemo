a3ddd8a7452e82befa267c9154ac0df6be849cbd
Gurleen kaur Gill <kau14748@sheridancollege.ca>
2023-06-19 17:23:50 -0400

    Commiting for the first time in part-timeStudent branch.

--- src/arraydemos23friam/StudentList.java
+++ src/arraydemos23friam/StudentList.java
@@ -10,8 +10,8 @@ public class StudentList {
         Student[] studentList = new Student[3];
         
         studentList[0]=s1;
-        studentList[1]=new Student("s2","abc");
-        studentList[2]=new Student("s3","def");
+        studentList[1]=new Student("12","abc");
+        studentList[2]=new Student("11","def");
         
         for(int i = 0;i<studentList.length;i++){
             System.out.println(studentList[i].getsName());
